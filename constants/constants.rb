e76a3f0009ca31eb64cd40040a8a26f2bf806ab7
Izhanov <ibekable@gmail.com>
2020-07-23 10:43:03 +0600

    Added seeds

---
+++ constants/constants.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+APPOLO_11 = {
+  mass: 28801,
+  paths: [
+    [:launch, 9.807],
+    [:land, 1.62],
+    [:launch, 1.62],
+    [:land, 9.807]
+  ]
+}.freeze
+
+MARS_MISSION = {
+  mass: 14606,
+  paths: [
+    [:launch, 9.807],
+    [:land, 3.711],
+    [:launch, 3.711],
+    [:land, 9.807],
+  ]
+}.freeze
+
+PASSENGER_SHIP = {
+  mass: 75432,
+  paths: [
+    [:launch, 9.807],
+    [:land, 1.62],
+    [:launch, 1.62],
+    [:land, 3.711],
+    [:launch, 3.711],
+    [:land, 9.807]
+  ]
+}.freeze
